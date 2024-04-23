79f8db61586e1a62faec7c42dd33d707d86d6a4a
naimuddin94 <developernaimuddin@gmail.com>
2024-04-24 01:08:15 +0600

    update user.routes.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,14 +2,14 @@
 const express = require('express');
 const {
   getAllDataFn,
-  getSingleDataFn,
-  updateFn,
   deleteFn,
   getUserRoleFn,
 } = require('../controller/shared.controller');
 const {
   registerUser,
   registerUserByAdmin,
+  updateUser,
+  getSingleUser,
 } = require('../controller/user.controller');
 const User = require('../models/user.model');
 const { verifyToken, verifyAdmin } = require('../middleware');
@@ -23,11 +23,12 @@ userRouter
 
 userRouter
   .route('/:id')
-  .get(getSingleDataFn(User))
-  .put(updateFn(User))
+  .get(getSingleUser)
+  .put(verifyToken, updateUser)
   .delete(verifyToken, verifyAdmin, deleteFn(User));
 userRouter.route('/role/:email').get(getUserRoleFn(User));
 
 userRouter.route('/admin/create').post(verifyToken, verifyAdmin, registerUserByAdmin);
+userRouter.route('/admin/update').put(verifyToken, verifyAdmin);
 
 module.exports = userRouter;
