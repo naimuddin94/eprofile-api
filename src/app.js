cd11db0acae0189975fbd644ee238d93a77e9e40
naimuddin94 <developernaimuddin@gmail.com>
2024-04-25 13:55:47 +0600

    update app.js

--- src/app.js
+++ src/app.js
@@ -24,7 +24,12 @@ const app = express();
 app.use(express.json());
 app.use(
     cors({
-        origin: ['http://localhost:5173', 'http://localhost:3000', 'https://eprofiles.vercel.app'],
+        origin: [
+            'http://localhost:5173',
+            'http://localhost:3000',
+            'https://eprofiles.vercel.app',
+            'https://eprofile-admin-dashboard.netlify.app',
+        ],
         credentials: true,
     })
 );
